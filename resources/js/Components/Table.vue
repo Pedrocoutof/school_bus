d122a6895b5f2f055ed9895b4e1c73b457d6e076
pedrocoutof <pedrocouto152001@gmail.com>
2024-05-19 21:31:20 -0300

    Melhorando visualização da tabela

--- resources/js/Components/Table.vue
+++ resources/js/Components/Table.vue
@@ -20,16 +20,17 @@ onMounted(async () => {
     <table class="table-auto w-full">
         <thead>
         <tr>
-            <th v-for="(thead, index) in theads" :class="{
-                'rounded-tl-lg': index === 0,
-                'rounded-tr-lg': index === theads.length - 1,
-            }"
-                class="text-gray-600 py-3 bg-gray-100">{{ thead }}</th>
+            <th v-for="(thead, index) in theads"
+                :class="{
+                    'rounded-tl-lg': index === 0,
+                    'rounded-tr-lg': index === theads.length - 1,
+                }"
+                class="text-gray-600 py-3 bg-gray-100 text-start pl-2">{{ thead }}</th>
         </tr>
         </thead>
         <tbody>
             <tr v-for="(row, index) in data" :key="index">
-                <td v-for="(value, key) in row" class="border-b border-slate-100 py-4" :key="key">{{ value }}</td>
+                <td v-for="(value, key) in row" class="border-b border-slate-100 py-4 pl-2" :key="key">{{ value }}</td>
             </tr>
         </tbody>
     </table>
