5fd17e100844afb3e3fa08596939baca1fb058b8
pedrocoutof <pedrocouto152001@gmail.com>
2024-06-07 14:09:12 -0300

    Instalando pacote de tradução

--- resources/js/Pages/Driver/Create.vue
+++ resources/js/Pages/Driver/Create.vue
@@ -88,8 +88,8 @@ function handleSubmit() {
 
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-            <button type="submit" class="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Save</button>
+            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancelar</button>
+            <button type="submit" class="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Salvar</button>
         </div>
     </form>
 </template>
